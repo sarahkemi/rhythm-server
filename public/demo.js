8f905c861404be3d0cc89f68a8f4882cec341746
Sarah Aladetan <sarahaladetan@gmail.com>
2016-07-15 14:03:06 -0400

    tried to emit a meeting event

---
+++ public/demo.js
@@ -0,0 +1,31 @@
+const Sibilant = require('../sibilant.js')
+const alert = require('alerts')
+const getUserMedia = require('getusermedia')
+const attachmediastream = require('attachmediastream')
+const socket = require('socketio')
+
+getUserMedia(function (err, stream) {
+  if (err) {
+    console.log('couldnt get user media!', err)
+    alert('Sibilant couldnt get your user media. give it access to audio and try again!')
+    throw err
+  } else {
+    console.log('got a stream!')
+    attachmediastream(stream, document.querySelector('#vid video'))
+    document.querySelector('#vid video').muted = true
+    var speakingEvents = new Sibilant(stream, {passThrough: false})
+    //make a new meeting, add person to meeting
+    socket.emit('meeting created', function(){
+      console.log('meeting has been created')
+    })
+    speakingEvents.bind('speaking', function () {
+      document.querySelector('#vid video').style.border = '10px solid #27ae60'
+      console.log('speaking!')
+      //send back speaking events to server
+    })
+
+    speakingEvents.bind('stoppedSpeaking', function (data) {
+      document.querySelector('#vid video').style.border = '10px solid #555'
+    })
+  }
+})
